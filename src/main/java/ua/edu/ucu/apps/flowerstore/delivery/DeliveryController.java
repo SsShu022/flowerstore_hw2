451592d0989763eeb184a3631b662d8765f9b0ed
SsShu022 <shuliak.pn@ucu.edu.ua>
2023-12-23 21:04:58 +0200

    edit

--- src/main/java/ua/edu/ucu/apps/flowerstore/delivery/DeliveryController.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/delivery/DeliveryController.java
@@ -1,6 +1,5 @@
 package ua.edu.ucu.apps.flowerstore.delivery;
 
-import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/ua/edu/ucu/apps/flowerstore/payment/PaymentController.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/payment/PaymentController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ua.edu.ucu.apps.flowerstore.orders.Item;
 
 @RestController
 @RequestMapping("/api/payment")
